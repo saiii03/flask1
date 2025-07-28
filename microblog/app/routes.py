62d94b848c5f4713e285e8038a40afb76ba2d87f
saiii03 <wsainath388@gmail.com>
2025-07-28 12:35:11 +0530

    2

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,12 +1,13 @@
 from app import app
-from flask import render_template
+from flask import render_template,flash,redirect,url_for
+from app.forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'sumit'}
-    posts = [
-        {'author': {'username': 'sumit'}, 'body': "the red is actually blue"},
-        {'author': {'username': 'sakshi'}, 'body': "colors is how you see it"}
-    ]
-    return render_template('index.html', title='home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        flash("login request for user{},remember_me={}".format(form.username.data , form.remember_me.data))
+        return redirect(url_for('index'))
+    
+    return render_template('login.html', title='login', form=form)
