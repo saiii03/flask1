64a5c2c936f392f2a500788e38906b69d3b433bc
saiii03 <wsainath388@gmail.com>
2025-07-28 12:31:30 +0530

    2

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,7 +1,12 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
-
 def index():
-    return "hii"
+    user = {'username': 'sumit'}
+    posts = [
+        {'author': {'username': 'sumit'}, 'body': "the red is actually blue"},
+        {'author': {'username': 'sakshi'}, 'body': "colors is how you see it"}
+    ]
+    return render_template('index.html', title='home', user=user, posts=posts)
